2bfc097695a9d20b4b2d4a10bce3199370ac4203
John Major <iamh2o@gmail.com>
2026-02-05 11:15:56 -0800

    Update Homebrew formula with release SHA256 for 0.1.0

--- Formula/wwk.rb
+++ Formula/wwk.rb
@@ -5,7 +5,7 @@ class Wwk < Formula
   desc "CLI for WellWhaddyaKnow time tracker"
   homepage "https://github.com/Daylily-Informatics/well-whaddya-know"
   url "https://github.com/Daylily-Informatics/well-whaddya-know/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
   head "https://github.com/Daylily-Informatics/well-whaddya-know.git", branch: "main"
 
